80a7240a127938999ca533b7e65827bade59dc81
Josh Wright <joshnotwright@gmail.com>
2021-09-17 11:18:00 -0400

    add some tests

--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,3 +1,35 @@
 package virtual_pet;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class VirtualPetTest {
+    private Dog underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Dog ("Wrigley", "Female", "Lab");
+    }
+
+    @Test
+    public void doesDogClassCreate() {
+        Dog underTest = new Dog("Wrigley", "Female", "Lab");
+    }
+
+    @Test
+    public void doesGetHungerReturn() {
+        assertEquals(20, underTest.getHungerLevel());
+    }
+
+    @Test
+    public void doesGetThirstReturn() {
+        assertEquals(20, underTest.getThirstLevel());
+    }
+
+    @Test
+    public void doesGetBoredomReturn() {
+        assertEquals(20, underTest.getBoredomLevel());
+    }
 }
